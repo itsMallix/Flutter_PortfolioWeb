7fa8031a28926cb12985505f13ac35bf61d225de
itsMallix <amandamaulana00@gmail.com>
2025-07-28 12:34:19 +0700

    add first commit

---
+++ lib/constant/theme.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class DesignSystem {
+  DesignSystem._();
+  static const Color darkTeal = Color(0xff004443);
+  static const Color normalTeal = Color(0xff01615f);
+  static const Color lightTeal = Color(0xff188f8b);
+  static const Color whiteteal = Color(0xfff0fdfa);
+  static const Color fadeTeal = Color(0xffccfbf1);
+
+  static const Color darkGray = Color(0xff374151);
+  static const Color lightGray = Color(0xff6b7280);
+
+  static const Color normalAmber = Color(0xffd4aa3a);
+  static const Color lightAmber = Color(0xffedcd5c);
+
+  static const TextTheme textTheme = TextTheme(
+    headlineLarge: headlineLarge,
+    headlineMedium: headlineMedium,
+    headlineSmall: headlineSmall,
+    titleLarge: titleLarge,
+    titleMedium: titleMedium,
+    titleSmall: titleSmall,
+    bodyLarge: bodyLarge,
+    bodyMedium: bodyMedium,
+    bodySmall: bodySmall,
+    labelLarge: labelLarge,
+    labelMedium: labelMedium,
+    labelSmall: labelSmall,
+  );
+
+  static const TextStyle headlineLarge = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: darkTeal,
+  );
+  static const TextStyle headlineMedium = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: darkTeal,
+  );
+  static const TextStyle headlineSmall = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: darkTeal,
+  );
+  static const TextStyle titleLarge = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: darkTeal,
+  );
+  static const TextStyle titleMedium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: darkTeal,
+  );
+  static const TextStyle titleSmall = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: darkTeal,
+  );
+  static const TextStyle bodyLarge = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: darkGray,
+  );
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: darkGray,
+  );
+  static const TextStyle bodySmall = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: darkGray,
+  );
+  static const TextStyle labelLarge = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: darkTeal,
+  );
+  static const TextStyle labelMedium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: darkTeal,
+  );
+  static const TextStyle labelSmall = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: darkTeal,
+  );
+}
+
+enum EditMode { ADD, UPDATE }
